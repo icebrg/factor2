d300eee72e5bcb754a2e9cf582408447b025d9cb
Brian Gilstrap <brian.gilstrap@ehi.com>
2016-08-11 14:46:27 -0500

    Get Spring Boot version working.

---
+++ src/main/java/net/gilstraps/brian/Factor2Application.java
@@ -0,0 +1,12 @@
+package net.gilstraps.brian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Factor2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Factor2Application.class, args);
+    }
+}
